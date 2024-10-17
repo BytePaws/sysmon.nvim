a21e390a12927abe6e8cab325e6c105b6507dc47
TiaraNivani <tiara.dev@proton.me>
2024-10-17 19:18:25 +0200

    Fix init.lua not finding module

--- lua/sysmon/init.lua
+++ lua/sysmon/init.lua
@@ -1,12 +1,13 @@
+-- Load the core system monitor functionality
 local sysmon = require("sysmon.sysmon")
 
--- Set statusline
-vim.o.statusline = "%!v:lua.require'statusline'.update_statusline()"
+-- Set the statusline in Neovim
+vim.o.statusline = "%!v:lua.require'sysmon.sysmon'.update_statusline()"
 
--- Update status line every 5 secs
+-- Update the system stats on CursorHold (trigger the updates periodically)
 vim.cmd([[
-	augroup UpdateStatusline
-		autocmd!
-		autocmd CursorHold,CursorHoldI * redrawstatus
-	augroup END
+  augroup UpdateStatusline
+    autocmd!
+    autocmd CursorHold,CursorHoldI * lua require('sysmon.sysmon').update_sys()
+  augroup END
 ]])
