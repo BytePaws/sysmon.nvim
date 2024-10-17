90379213765ee4b0d3647140489851f82ac78e98
TiaraNivani <tiara.dev@proton.me>
2024-10-17 18:53:04 +0200

    Asynchronously gather data and update status bar

--- lua/sysmon.lua
+++ lua/sysmon.lua
@@ -1,27 +1,110 @@
 local M = {}
 
+-- Cache last values
+local cpu_c, mem_c, temp_c = "", "", ""
+local last_update = 0
+local interval = 5000 -- 5000 milliseconds (5 secs)
+
+-- Execute shell commands asynchronously
+local function run_command(cmd, callback)
+    local handle = vim.loop.spawn("bash", {
+        args = { "-c", cmd },
+        stdio = { nil, vim.loop.new_pipe(false), vim.loop.new_pipe(false) },
+    }, function(code, signal)
+        if code ~= 0 then
+            callback(nil, code, signal)  -- Send error code if command fails
+        end
+        handle:close()
+    end)
+
+    local stdout = handle:get_stdout()
+    local output = ""
+    stdout:read_start(function(err, data)
+        if data then
+            output = output .. data
+        end
+        if err then
+            callback(nil, err)
+        elseif data == nil then
+            callback(output, nil)  -- Send the command output
+        end
+    end)
+end
 
 function M.get_cpu_usage()
-	local cpu_usage = io.popen("top -bn1 | grep 'Cpu(s)' | awk '{print $2 + $4}'"):read("*a")
-	return string.format("CPU: %.2f%%", tonumber(cpu_usage) or 0)
+	run_command("top -bn1 | grep 'Cpu(s)' | awk '{print $2 + $4}'", function(output, err)
+		if err then
+			callback("CPU: N/A")
+		else
+			local cpu = tonumber(output)
+			if cpu then
+				callback(string.format("CPU: %.2f%%", cpu))
+			else
+				callback("CPU: N/A")
+			end
+		end
+	end)
 end
 
 function M.get_mem_usage()
-	local mem_info = io.popen("free -m | grep Mem | awk '{print $3/$2 *100.0}'"):read("*a")
-	return string.format("RAM: %.2f%%", tonumber(mem_info) or 0)
+	run_command("free -m | grep Mem | awk '{print $3/$2 * 100.0}'", function(output, err)
+		if err then
+			callback("Mem: N/A")
+		else
+			local mem = tonumber(output)
+			if mem then
+				callback(string.format("Mem: %.2f%%", mem))
+			else
+				callback("Mem: N/A")
+			end
+		end
+	end)
 end
 
 function M.get_sys_temp()
-	local temp = io.popen("sensors | grep 'Package id 0:' | awk '{print $4}'"):read("*a")
-	return "Temp: " .. (temp or "N/A")
+	run_command("sensors | grep 'Package id 0:' | awk '{print $4}'", function(output, err)
+		if err then
+			callback("Temp N/A")
+		else
+			local temp = output.match("%d+%.%d+")
+			if temp then
+				callback("Temp: " .. temp .. "°C")
+			else
+				callback("Temp: N/A")
+			end
+		end
+	end)
+end
+
+-- Update system stats asynchronously
+local function update_sys()
+	local time = vim.loop.now()
+	if time - last_update < interval then
+		return -- Don't update if the last one was too recent
+	end
+
+	last_update = time
+
+	get_cpu_usage(function(cpu)
+		cpu_c = cpu
+		vim.cmd("redrawstatus") -- Force statusline redraw
+	end)
+
+	get_mem_usage(function(mem)
+		mem_c = mem
+		vim.cmd("redrawstatus")
+	end)
+
+	get_sys_temp(function (temp)
+		temp_c = temp
+		vim.cmd("redrawstatus")
+	end)
 end
 
 function M.update_statusline()
-	local cpu = M.get_cpu_usage()
-	local mem = M.get_mem_usage()
-	local temp = M.get_sys_temp()
+	update_sys()
 
-	return table.concat({ cpu, mem, temp }, " | ")
+	return table.concat({ cpu_c, mem_c, temp_c }, " | ")
 end
 
 -- Set statusline
